0b9162faceca2d0cb3263ed5feea95779aff9b61
Dmitry Burlakov <hi@dantaeusb.me>
2023-02-06 04:26:03 +0300

    Best homemade Minecraft font in existence

--- src/components/layouts/wrapper/layout-wrapper.component.tsx
+++ src/components/layouts/wrapper/layout-wrapper.component.tsx
@@ -6,23 +6,7 @@ const FontSupportedLocales: Locale[] = ['en'];
 const LayoutWrapper = ({
   children,
 }: PropsWithChildren<Record<string, unknown>>) => {
-  return (
-    <LocaleContext.Consumer>
-      {(localeContext) => {
-        return (
-          <span
-            className={
-              FontSupportedLocales.includes(localeContext.locale)
-                ? 'pixel-font'
-                : 'sans-serif-font'
-            }
-          >
-            {children}
-          </span>
-        );
-      }}
-    </LocaleContext.Consumer>
-  );
+  return (children);
 };
 
 export default LayoutWrapper;
